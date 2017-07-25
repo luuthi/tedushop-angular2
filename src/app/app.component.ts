7e790541b2b0a1d970191485486ebbc45bf162c5
luu dinh thi <luudinhthi22t.t@gmail.com>
2017-07-26 00:08:25 +0700

    add home UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewChecked, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements AfterViewChecked {
+  constructor(private elementRef: ElementRef) {
+
+  }
+  ngAfterViewChecked() {
+    //Called after every check of the component's or directive's content.
+    //Add 'implements AfterContentChecked' to the class.
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "../assets/js/custom.js";
+    this.elementRef.nativeElement.appendChild(script);
+  }
 }
